eb4c537521314fb91d261242b70b482a01122bb0
Alexander Nicholson <alexn400@gmail.com>
2020-09-17 21:07:30 +1200

    attempt to fix swagger docs

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
+const path = require('path');
 const userRoute = require('./routes/user');
 const recipeRoute = require('./routes/recipe');
 require('./util/mongoose');
@@ -14,21 +15,24 @@ const swaggerOptions = {
       description: 'Documentation for Braize. Go to http://braize.azurewebsites.net/ to see it in action',
     },
   },
-  apis: ['./src/routes/*.js'],
+  apis: [path.join(__dirname, '/routes/*.js')],
 };
 
 const app = express();
+
+console.log(path.join(__dirname, '/routes/*.js'));
 
 // Parse incoming JSON
 app.use(express.json());
 app.use(cors());
 
 // Swagger endpoint
-const swaggerDoc = swaggerJSDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // Routers
 app.use(userRoute);
 app.use(recipeRoute);
+
+const swaggerDoc = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 module.exports = app;
